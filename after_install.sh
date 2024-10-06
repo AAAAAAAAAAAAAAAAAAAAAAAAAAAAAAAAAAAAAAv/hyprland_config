20310982e119a7956f4826bdbce1fdfba2ec3eb3
Louis <Louis.Pean@eleves.ec-nantes.fr>
2024-10-06 15:29:24 +0200

    added install scripts for base install
    it adds everything needed to install the base things needed for hyprland. Will need to be refined in the future

---
+++ after_install.sh
@@ -0,0 +1,59 @@
+
+sudo nano /etc/apt/sources.list
+sudo apt update
+sudo apt install -y apt-listbugs apt-listchanges
+#window manager:
+sudo apt install -y hyprland
+#session manager:
+sudo apt install --no-install-recommends -y sddm
+sudo apt install -y firmware-amd-graphics brightnessctl playerctl
+sudo apt install -y pipewire wireplumber
+sudo apt install -y tilix
+sudo apt install -y sway-notification-center
+sudo apt install -y xdg-desktop-portal-hyprland
+sudo apt install -y qtwayland5 qt6-wayland
+sudo apt install -y polkit-kde-agent-1
+sudo apt install -y wofi rofi waybar
+sudo apt install --no-install-recommends -y network-manager-gnome
+sudo apt install -y nwg-look
+
+
+
+
+
+#software
+sudo apt install -y easyeffects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
